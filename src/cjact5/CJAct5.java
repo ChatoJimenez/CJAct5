9491b45ffc1b9911e6ff526035010b308afac2b9
Jesus Jimenez <jesgui99@gmail.com>
2020-09-06 18:33:08 -0500

    commit actividad5

---
+++ src/cjact5/CJAct5.java
@@ -0,0 +1,26 @@
+package cjact5;
+/**
+ *
+ * @author jesgu
+ */
+public class CJAct5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Atributos numéricos (variables)
+        int x = 10;
+        int y = 4;
+        //Impresión de datos
+        System.out.println("X = " + x);
+        System.out.println("Y = " + y);
+        //Operaciones aritméticas
+        System.out.println("Suma: " + (x + y));
+        System.out.println("Resta: " + (x - y));
+        System.out.println("Multiplicación: " + x * y);
+        System.out.println("División: " + (double) x / y);
+        System.out.println("Módulo: " + x % y);
+    }
+    
+}
